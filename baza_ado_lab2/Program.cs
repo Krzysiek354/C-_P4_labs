0228e6d34c131cbc0d1554a43717688337ccdb36
Krzysiek <krzwyr25@gmail.com>
2022-03-16 20:00:06 +0100

    Dodaj pliki projektów.

---
+++ baza_ado_lab2/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using baza_ado_lab2;
+
+string connectionString = @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=Northwind2;Integrated Security=True;Pooling=False";
+using (SqlConnection connection = new SqlConnection(connectionString))
+{
+    connection.Open();
+    using (SqlCommand komenda = new SqlCommand("SELECT * FROM dbo.[Region]", connection))
+    {
+        SqlDataReader reader = komenda.ExecuteReader();
+
+
+        if (reader.HasRows)
+        {
+            while (reader.Read())
+            {
+                Console.WriteLine("{0}\t{1}", reader.GetInt32(0),
+                    reader.GetString(1));
+            }
+        }
+        else
+        {
+            Console.WriteLine("No rows found.");
+        }
+        reader.Close();
+    }
+
+    using (SqlCommand komenda = new SqlCommand("INSERT Region (RegionID, RegionDescription) VALUES (5, 'NorthEastern')", connection))
+    {
+        SqlDataReader reader = komenda.ExecuteReader();
+        reader.Close();
+    }
+    connection.Close();
+}
+
+
+
+    Console.ReadKey();
---
+++ baza_ado_lab2/Region.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baza_ado_lab2
+{
+    internal class Region
+    {
+        public Region(int id, string reg)
+        {
+            RegionID = id;
+            RegionDescription = reg;
+        }
+        public int RegionID
+        {
+            get;
+            set;
+        }
+        public string RegionDescription
+        {
+            get;
+            set;
+        }
+    }
+}
