ace08ae34aee2a3a94f9db82baf77c0711f6a820
Krzysiek <krzwyr25@gmail.com>
2022-05-31 09:30:02 +0200

    poprawa

--- zadanie_lab_5/Program.cs
+++ zadanie_lab_5/Program.cs
@@ -28,83 +28,97 @@ while (true)
     zm = Console.ReadLine();
 
 
-
-    if (zm == "1")
+    switch (zm)
     {
-        var data = new Autors();
-        var data_book = new Books();
-        Console.WriteLine("DODAWANIE DANYCH:");
-        Console.WriteLine("Autor Imie:");
-        data.Name = Console.ReadLine();
-        Console.WriteLine("Autor Nazwisko:");
-        data.Surname = Console.ReadLine();
-        Console.WriteLine("Ksiazka Tytul:");
-        data_book.Title = Console.ReadLine();
-        Console.WriteLine("Ksiazka Rok:");
-        data_book.Year =  Convert.ToInt32(Console.ReadLine());
-        data.Book.Add(data_book);
+        case "1":
+            var data = new Autors();
+            var data_book = new Books();
+            Console.WriteLine("DODAWANIE DANYCH:");
+            Console.WriteLine("Autor Imie:");
+            data.Name = Console.ReadLine();
+            Console.WriteLine("Autor Nazwisko:");
+            data.Surname = Console.ReadLine();
+            Console.WriteLine("Ksiazka Tytul:");
+            data_book.Title = Console.ReadLine();
+            Console.WriteLine("Ksiazka Rok:");
+            data_book.Year = Convert.ToInt32(Console.ReadLine());
+            data.Book.Add(data_book);
 
-        context.Autors.Add(data);
-        context.SaveChanges();
+            context.Autors.Add(data);
+            context.SaveChanges();
 
-        Console.ReadKey();
-    }
-    else if (zm == "2")
-    {
-        Console.WriteLine("SZUKASZ AUTORA Nazwisko:");
-        autor = Console.ReadLine();
-        var aut0r = context.Autors.Include(x=>x.Book);
-        
-     
-        
-        Console.WriteLine("Wyniki:");
-       
-      
-        
-            foreach (var item in aut0r.Where(x=>x.Surname==autor))
+            Console.ReadKey();
+            break;
+
+
+        case "2":
+
+
+            Console.WriteLine("SZUKASZ AUTORA Nazwisko:");
+            autor = Console.ReadLine();
+            var aut0r = context.Autors.Include(x => x.Book);
+
+
+
+            Console.WriteLine("Wyniki:");
+
+
+
+            var item = aut0r.Where(x => x.Surname == autor);
+
+            foreach (var it in item)
             {
-                foreach (var it in item.Book)
+                Console.WriteLine($"{it.Name} {it.Surname}");
+                Console.WriteLine("TYTULY");
+                foreach (var i in it.Book)
                 {
-                    Console.WriteLine($"{it.Autor.Name} {it.Autor.Surname}  {it.Title}  {it.Year}");
+                    Console.WriteLine($"{i.Title}");
                 }
+                Console.WriteLine("  ");
             }
 
-        
+
+            Console.ReadKey();
+
+            break;
+
+        case "3":
+
+
+            Console.WriteLine("SZUKASZ KSIAZKI Tytul:");
+
+
+            ksiazka = Console.ReadLine();
+            var b00k = context.Books.Include(x => x.Autor).ToArray();
 
 
 
-        Console.ReadKey();
-    }
-    else if (zm == "3")
-    {
-        Console.WriteLine("SZUKASZ KSIAZKI Tytul:");
+            Console.WriteLine("Wyniki:");
 
-       
-        ksiazka = Console.ReadLine();
-        var b00k = context.Books.Include(x => x.Autor).ToArray();
+            var iteme = b00k.Where(x => x.Title == ksiazka);
 
-
-
-        Console.WriteLine("Wyniki:");
-
-            foreach (var item in b00k.Where(x => x.Title == ksiazka))
+            foreach (var idd in iteme)
             {
-                Console.WriteLine($"{item.Title} { item.Year}");
+                Console.WriteLine($"{idd.Title} { idd.Year}");
             }
 
-        
 
-        Console.ReadKey();
+
+            Console.ReadKey();
+
+            break;
+
+        case "4":
+            return;
+
+
+        default:
+            Console.WriteLine("Niepoprawny wybor");
+            Console.ReadKey();
+            break;
 
     }
-    else if (zm == "4")
-    {
-        break;
-    }
-    else
-    {
-        Console.WriteLine("Niepoprawny wybor");
-        Console.ReadKey();
-    }
+
     System.Console.Clear();
+
 }
---
+++ zajecia_7/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zajecia_7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public class nowa:jakis
+    {
+       public void meta()
+        {
+
+        }
+    }
+    public interface jakis
+    { void meta(); }
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        jakis zm= new nowa();
+
+
+    }
+}
